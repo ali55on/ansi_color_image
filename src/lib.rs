ffe486cbac018cd70984a058cf6c915b0f0caad3
w-a-gomes <wildenalisson@outlook.com>
2022-01-23 22:44:12 -0300

    Map to Vec

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+// use std::collections::HashMap;
 
 use image::{
     GenericImageView, open, Rgba,  // DynamicImage
@@ -7,7 +7,7 @@ use image::{
 
 #[derive(Debug)]
 pub struct ImageColorMap {
-    pixels_map: HashMap<u32, Vec<(String, (u8, u8, u8))>>,
+    pixels_map: Vec<Vec<(String, (u8, u8, u8))>>,
 }
 
 impl ImageColorMap {
@@ -65,66 +65,64 @@ impl ImageColorMap {
             new_image = image::imageops::colorops::brighten(&new_image, b);
         }
 
-        // Criar um 'Mapa' com vetores de tuplas com rgb. Ex:
-        // {
-        //      1: [Rgba, Rgba, Rgba],
-        //      2: [Rgba, Rgba, Rgba],
-        // }
+        // Criar um 'Mapa' com linhas/vetores de rgb. Ex:
+        // [
+        //      [Rgba, Rgba, Rgba],
+        //      [Rgba, Rgba, Rgba],
+        // ]
         // Cada Vetor é uma linha da imagem.
         // Cada Rgba, é uma informação de cor (um pixel).
-        let mut map: HashMap<u32, Vec<Rgba<u8>>> = HashMap::new();
+        // let mut map: HashMap<u32, Vec<Rgba<u8>>> = HashMap::new();
+        let mut map: Vec<Vec<Rgba<u8>>> = Vec::new();
         let mut count = 1;
-        let mut line = 1;
+        let mut line = 0;
 
         // Iterar pixels
-        for pixel in new_image.pixels() {
-            //let pixel = pixel;  // pixel = (u32, u32, Rgba<u8>)
-
-            // Se linha existe
-            if let Some(_) = map.get(&line) {
-                // Atualiza o valor que vai na key da hash
-                let mut v = map.get(&line).unwrap().to_vec();
-                v.push(*pixel);  
-
-                // Atualiza a hash
-                map.insert(line, v);
-
-            // Se não, insere uma nova key/linha e valor
+        for &pixel in new_image.pixels() {
+            if count == 1 {
+                map.push(vec![pixel]);
             } else {
-                map.insert(line, vec![*pixel]);
+                map[line].push(pixel);    
             }
-            
-            if Some(count) == width {
+
+            if count == width.unwrap() as usize {
                 count = 1;
                 line += 1;
             } else {
                 count += 1;
             }
-
         }
-
+        
         // Mapa de retorno
-        let mut pixels_map: HashMap<u32, Vec<(String, (u8, u8, u8))>> = HashMap::new();
+        let mut pixels_map: Vec<Vec<(String, (u8, u8, u8))>> = Vec::new();
+        let mut count = 1;
+        let mut line = 0;
 
-        for line_num in 1..(width.unwrap() + 1) {
-            let mut pixels_line: Vec<(String, (u8, u8, u8))> = Vec::new();
+        for map_line in map {
+            for pixel_info in map_line {
+                let rgba = pixel_info;  // Rgba([3, 15, 19, 14])
+                let [r, g, b, _a] = rgba.0;
+                
+                // https://github.com/EbonJaeger/asciifyer/blob/main/src/lib.rs
+                let brightness = (
+                    (0.2126 * r as f64) + (0.7152 * g as f64) + (0.0722 * b as f64)) as f64;
 
-            if let Some(image_line) = map.get(&line_num) {
-                for pixel_info in image_line {  // pixel_info = Rgba([1, 2, 3])
-                    let rgba = pixel_info;
-                    let [r, g, b, _a] = rgba.0;
+                let ascii_chars = [" ", " ", ":", "i", "/", "n", "k", "m", "0", "@", "#"];
+                let ascii_chars_index =(
+                    (brightness / 255.0) * (ascii_chars.len() - 1) as f64).round() as usize;
 
-                    // https://github.com/EbonJaeger/asciifyer/blob/main/src/lib.rs
-                    let brightness = (
-                        (0.2126 * r as f64) + (0.7152 * g as f64) + (0.0722 * b as f64)) as f64;
-
-                    let ascii_chars = [" ", " ", ":", "i", "/", "n", "k", "m", "0", "@", "#"];
-                    let ascii_chars_index =(
-                        (brightness / 255.0) * (ascii_chars.len() - 1) as f64).round() as usize;
-
-                    pixels_line.push((String::from(ascii_chars[ascii_chars_index]), (r, g, b)));
+                if count == 1 {
+                    pixels_map.push(vec![(String::from(ascii_chars[ascii_chars_index]), (r, g, b))]);
+                } else {
+                    pixels_map[line].push((String::from(ascii_chars[ascii_chars_index]), (r, g, b)));
                 }
-                pixels_map.insert(line_num, pixels_line);
+
+                if count == width.unwrap() as usize {
+                    line += 1;
+                    count = 1;
+                } else {
+                    count += 1;
+                }
             }
         }
 
@@ -134,7 +132,7 @@ impl ImageColorMap {
         }
     }
 
-    pub fn pixels_map(&self) -> &HashMap<u32, Vec<(String, (u8, u8, u8))>> {
+    pub fn pixels_map(&self) -> &Vec<Vec<(String, (u8, u8, u8))>> {
         &self.pixels_map
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use ansi_term::Colour::RGB;
 use ansi_color_image;
 
 fn main() {
-    let width = 80;
-    let height = 39;
+    let width = 40;
+    let height = 20;
 
     // Create Map
     let image_map = ansi_color_image::ImageColorMap::new(
@@ -19,13 +19,15 @@ fn main() {
     let pixels_map = image_map.pixels_map();
 
     // Print
-    for line_num in 1..(height + 1) {
-        if let Some(image_line) = pixels_map.get(&line_num) {
-            for pixel_info in image_line {
-                let (c, rgb) = pixel_info;
-                print!("{}", RGB(rgb.0, rgb.1, rgb.2).paint(c));
-            }
+    // for line_num in 1..(height + 1) {
+    for image_line in pixels_map {
+        for pixel_info in image_line {
+            let (c, rgb) = pixel_info;
+            print!("{}", RGB(rgb.0, rgb.1, rgb.2).paint(c));
         }
         println!();
     }
+    
+    
+    
 }
